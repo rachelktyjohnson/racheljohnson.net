4eb195bcd54cef6abaf8072253c9f5bf3f068472
Rachel Johnson <rachelktyjohnson@gmail.com>
2024-04-23 23:10:02 +1000

    add designer background

--- script.js
+++ script.js
@@ -1,54 +1,21 @@
-// // document.querySelectorAll('.facet-header span').forEach((element)=>{
-// //     element.style.display="none";
-// // })
-//
-// document.querySelectorAll('.current').forEach((element)=>{
-//     element.style.display="none";
-// })
-//
-// document.querySelector("#educator").innerText = "educate";
-//
-// let design_open = false
-// let develop_open = false
-// let learn_open = false
-// let educate_open = false
-
-// document.querySelector(".content").addEventListener('mouseover',(e)=>{
-//     if(e.target.closest('.facet-header')){
-//         if (e.target.closest('.facet-header').firstElementChild.id==="educator"){
-//             e.target.closest('.facet-header').firstElementChild.innerText="educat"
-//         }
-//         // e.target.closest('.facet-header').lastElementChild.style.display = "block";
-//         e.target.closest('.facet-header').lastElementChild.classList.add("show")
-//     }
-//
-// document.querySelector(".content").addEventListener('mouseout',(e)=>{
-//     if(e.target.closest('.facet-header')){
-//         if (e.target.closest('.facet-header').firstElementChild.id==="educator"){
-//             e.target.closest('.facet-header').firstElementChild.innerText="educate"
-//         }
-//         e.target.closest('.facet-header').lastElementChild.style.display = "none";
-//         e.target.closest('.facet-header').lastElementChild.classList.remove("show")
-//     }
-// })
-//
-// document.querySelector(".content").addEventListener('click',(e)=>{
-//     if (e.target.id){
-//         document.querySelectorAll('.current').forEach((el)=>{
-//             el.style.display="none";
-//         })
-//         e.target.parentNode.parentNode.lastElementChild.style.display="block";
-//     }
-// })
-
 document.querySelector(".content").addEventListener('click',(e)=> {
     if (e.target.closest('.facet-header')) {
+
+        document.querySelector("body").className="";
         if( e.target.closest('.facet-header').classList.contains("facet-open")){
             e.target.closest('.facet-header').classList.remove("facet-open")
             e.target.closest('.facet-header').nextElementSibling.classList.remove("body-open")
+            document.querySelector("body").classList.remove('body-designer')
         } else {
+            document.querySelectorAll('.facet-header').forEach((element)=>{
+                element.classList.remove("facet-open");
+                element.nextElementSibling.classList.remove("body-open")
+            })
             e.target.closest('.facet-header').classList.add("facet-open")
             e.target.closest('.facet-header').nextElementSibling.classList.add("body-open")
+            if (e.target.closest('.facet-header').firstElementChild.id==="designer"){
+                document.querySelector("body").classList.add('body-designer')
+            }
         }
 
     }
