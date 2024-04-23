19607dac4c391ffe64048b2b75acad45d0f9f3e1
Rachel Johnson <rachelktyjohnson@gmail.com>
2024-04-23 17:46:22 +1000

    no more buggy hover

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.querySelectorAll('.facet-header span').forEach((element)=>{
-    element.style.display="none";
-})
+// document.querySelectorAll('.facet-header span').forEach((element)=>{
+//     element.style.display="none";
+// })
 
 document.querySelectorAll('.current').forEach((element)=>{
     element.style.display="none";
@@ -8,21 +8,29 @@ document.querySelectorAll('.current').forEach((element)=>{
 
 document.querySelector("#educator").innerText = "educate";
 
+let design_open = false
+let develop_open = false
+let learn_open = false
+let educate_open = false
+
 document.querySelector(".content").addEventListener('mouseover',(e)=>{
-    if (e.target.id){
-        e.target.nextElementSibling.style.display="inline";
-        if (e.target.id==="educator"){
-            e.target.innerText = "educat"
+    if(e.target.closest('.facet-header')){
+        if (e.target.closest('.facet-header').firstElementChild.id==="educator"){
+            e.target.closest('.facet-header').firstElementChild.innerText="educat"
         }
+        e.target.closest('.facet-header').lastElementChild.style.display = "block";
+        e.target.closest('.facet-header').lastElementChild.classList.add("show")
     }
+
 })
 
 document.querySelector(".content").addEventListener('mouseout',(e)=>{
-    if (e.target.id){
-        e.target.nextElementSibling.style.display="none";
-        if (e.target.id==="educator"){
-            e.target.innerText = "educate"
+    if(e.target.closest('.facet-header')){
+        if (e.target.closest('.facet-header').firstElementChild.id==="educator"){
+            e.target.closest('.facet-header').firstElementChild.innerText="educate"
         }
+        e.target.closest('.facet-header').lastElementChild.style.display = "none";
+        e.target.closest('.facet-header').lastElementChild.classList.remove("show")
     }
 })
 
