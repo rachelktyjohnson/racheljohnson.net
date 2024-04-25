f875e9d5fda162e4e2ba23931700f3b41c62b1c3
Rachel Johnson <rachelktyjohnson@gmail.com>
2024-04-25 20:01:34 +1000

    facets open, close, and toggle between each other

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,118 @@
-function triggerFlair(triggerBool){
-    if (triggerBool){
-        anime({
-            targets: '.flair',
-            translateY: [500, 0],
-            duration: 1000,
-        })
+function triggerHoverFlair(triggerBool, target) {
+    if (!anyFlairOpen()){
+        if (triggerBool) {
+            //if we want it to show
+            //but we need to check if someone else is already open
+            anime({
+                targets: target,
+                translateY: [500, 0],
+                duration: 1000,
+            })
+        } else {
+            //if we want flair to hide
+            anime({
+                targets: target,
+                translateY: [0, 500],
+                duration: 1000,
+            })
+        }
+    }
+
+}
+
+function anyFlairOpen(){
+    let flairOpen = false;
+    document.querySelectorAll('.flair').forEach((element)=>{
+        console.log(element.classList)
+        if (element.classList.contains('showing')){
+            flairOpen = true
+        }
+    })
+    return flairOpen;
+}
+
+//HOVER EFFECTS
+//START
+
+document.querySelector("#designer").addEventListener('mouseover', (e) => {
+    if (!anyFlairOpen()) {
+        triggerHoverFlair(true, '.flair-designer')
+    }
+})
+document.querySelector("#designer").addEventListener('mouseout', (element) => {
+        triggerHoverFlair(false, '.flair-designer')
+
+
+})
+
+document.querySelector("#developer").addEventListener('mouseover', (element) => {
+    if (!anyFlairOpen()) {
+        triggerHoverFlair(true, '.flair-developer')
+    }
+})
+document.querySelector("#developer").addEventListener('mouseout', (element) => {
+    triggerHoverFlair(false, '.flair-developer')
+})
+
+document.querySelector("#learner").addEventListener('mouseover', (element) => {
+    if (!anyFlairOpen()) {
+        triggerHoverFlair(true, '.flair-learner')
+    }
+})
+document.querySelector("#learner").addEventListener('mouseout', (element) => {
+    triggerHoverFlair(false, '.flair-learner')
+})
+document.querySelector("#educator").addEventListener('mouseover', (element) => {
+    if (!anyFlairOpen()) {
+        triggerHoverFlair(true, '.flair-educator')
+    }
+})
+document.querySelector("#educator").addEventListener('mouseout', (element) => {
+    triggerHoverFlair(false, '.flair-educator')
+})
+
+//END
+//HOVER EFFECTS
+
+document.querySelector("#designer").addEventListener('click', toggleFacet);
+document.querySelector("#developer").addEventListener('click', toggleFacet);
+document.querySelector("#learner").addEventListener('click', toggleFacet);
+document.querySelector("#educator").addEventListener('click', toggleFacet);
+
+function toggleFacet(e) {
+    if (e.target.classList.contains('facet-open')) {
+        closeFacet(e.target)
     } else {
-        anime({
-            targets: '.flair',
-            translateY: [0, 500],
-            duration: 1000,
-        })
+
+        //close any other facets
+        if(document.querySelector('.facet-open')){
+            console.log("hi");
+            closeFacet(document.querySelector('.facet-open'))
+        }
+
+        openFacet(e.target)
+
     }
 }
 
-document.querySelector("#designer").addEventListener('mouseover',(element)=>{
-    document.querySelector('.flair').style.backgroundImage = "url('../img/des.png')";
-    triggerFlair(true)
-})
-document.querySelector("#designer").addEventListener('mouseout',(element)=>{
-    triggerFlair(false)
-})
+function closeFacet(el) {
+    el.parentElement.nextElementSibling.classList.remove('body-open')
+    el.classList.remove('facet-open');
+    const flair_showing = document.querySelector(".flair.showing");
+    flair_showing.classList.remove('showing')
+    //trigger thingy
+    triggerHoverFlair(false, flair_showing)
+    console.log(el.id)
+}
 
-document.querySelector("#developer").addEventListener('mouseover',(element)=>{
-    document.querySelector('.flair').style.backgroundImage = "url('../img/dev.png')";
-    triggerFlair(true)
-})
-document.querySelector("#developer").addEventListener('mouseout',(element)=>{
-    triggerFlair(false)
-})
-
-document.querySelector("#learner").addEventListener('mouseover',(element)=>{
-    document.querySelector('.flair').style.backgroundImage = "url('../img/learn.png')";
-    triggerFlair(true)
-})
-document.querySelector("#learner").addEventListener('mouseout',(element)=>{
-    triggerFlair(false)
-})
-document.querySelector("#educator").addEventListener('mouseover',(element)=>{
-    document.querySelector('.flair').style.backgroundImage = "url('../img/ed.png')";
-    triggerFlair(true)
-})
-document.querySelector("#educator").addEventListener('mouseout',(element)=>{
-    triggerFlair(false)
-})
+function openFacet(el) {
+    el.parentElement.nextElementSibling.classList.add('body-open')
+    el.classList.add('facet-open')
+    console.log(el.id)
+    const flair_to_open = document.querySelector(`.flair-${el.id}`)
+    triggerHoverFlair(true, flair_to_open)
+    flair_to_open.classList.add('showing');
+}
 
 let timeline = anime.timeline({
     easing: 'easeOutExpo',
@@ -62,6 +131,6 @@ timeline
     }, "-=500")
     .add({
         targets: '.facet',
-        translateY: [800,0],
+        translateY: [800, 0],
         delay: anime.stagger(100),
     })
